4f87ad4a9aadea352c90c4074dc5a078be3e3746
valdo0 <sebastian.valdivia97gmail.com>
2025-06-23 20:15:31 -0400

    Se añaden pruebas unitarias para RecuperarPwComponent con módulos y proveedores necesarios

--- src/app/pages/recuperar-pw/recuperar-pw.component.spec.ts
+++ src/app/pages/recuperar-pw/recuperar-pw.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RecuperarPwComponent } from './recuperar-pw.component';
+import { ComponentsModule } from 'src/app/components/layout/components.module';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('RecuperarPwComponent', () => {
   let component: RecuperarPwComponent;
@@ -8,7 +11,22 @@ describe('RecuperarPwComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [RecuperarPwComponent]
+      declarations: [RecuperarPwComponent],
+      imports: [ComponentsModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: (key: string) => null,
+              },
+            },
+            params: of({}),
+            queryParams: of({}),
+          },
+        },
+      ],
     });
     fixture = TestBed.createComponent(RecuperarPwComponent);
     component = fixture.componentInstance;
