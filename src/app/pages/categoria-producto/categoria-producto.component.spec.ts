cc18cb89a536a882a02bca56f555b7f467b83507
valdo0 <sebastian.valdivia97gmail.com>
2025-06-23 20:15:22 -0400

    Se añaden pruebas unitarias para CategoriaProductoComponent con módulos y proveedores necesarios

--- src/app/pages/categoria-producto/categoria-producto.component.spec.ts
+++ src/app/pages/categoria-producto/categoria-producto.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CategoriaProductoComponent } from './categoria-producto.component';
+import { ComponentsModule } from 'src/app/components/layout/components.module';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('CategoriaProductoComponent', () => {
   let component: CategoriaProductoComponent;
@@ -8,7 +11,22 @@ describe('CategoriaProductoComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CategoriaProductoComponent]
+      declarations: [CategoriaProductoComponent],
+      imports: [ComponentsModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: (key: string) => null,
+              },
+            },
+            params: of({}),
+            queryParams: of({}),
+          },
+        },
+      ],
     });
     fixture = TestBed.createComponent(CategoriaProductoComponent);
     component = fixture.componentInstance;
