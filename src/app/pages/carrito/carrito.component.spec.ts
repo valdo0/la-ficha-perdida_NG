191449167dc1ba212fb33674b8be57b5ae42bd64
valdo0 <sebastian.valdivia97gmail.com>
2025-06-23 20:15:17 -0400

    Se añaden pruebas unitarias para CarritoComponent con módulos y proveedores necesarios

--- src/app/pages/carrito/carrito.component.spec.ts
+++ src/app/pages/carrito/carrito.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CarritoComponent } from './carrito.component';
+import { ComponentsModule } from 'src/app/components/layout/components.module';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('CarritoComponent', () => {
   let component: CarritoComponent;
@@ -8,7 +11,22 @@ describe('CarritoComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CarritoComponent]
+      declarations: [CarritoComponent],
+      imports:[ComponentsModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: (key: string) => null
+              }
+            },
+            params: of({}),
+            queryParams: of({})
+          }
+        }
+      ]
     });
     fixture = TestBed.createComponent(CarritoComponent);
     component = fixture.componentInstance;
