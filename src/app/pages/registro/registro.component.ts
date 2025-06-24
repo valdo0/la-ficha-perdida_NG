1e22eb1b76050c5be13019e3928cc7bd79423d5d
valdo0 <sebastian.valdivia97gmail.com>
2025-06-23 20:15:42 -0400

    Se mejora el formulario de registro de usuario con validaciones de edad mínima y validación de contraseñas coincidentes.

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -2,22 +2,23 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { minAgeValidator, passwordsIgualesValidator } from 'src/app/validators/custom-validators';
 import Swal from 'sweetalert2';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent {
   registroForm!: FormGroup;
 
   username: string = '';
-  nombre:string ='';
-  fecNacimiento:string='';
-  direccion:string='';
-  correo:string='';
+  nombre: string = '';
+  fecNacimiento: string = '';
+  direccion: string = '';
+  correo: string = '';
   password: string = '';
-  confirmPassword:string='';
+  confirmPassword: string = '';
 
   constructor(
     private authService: AuthService,
@@ -25,37 +26,62 @@ export class RegistroComponent {
     private fb: FormBuilder
   ) {}
   ngOnInit(): void {
-    this.registroForm = this.fb.group({
-      username: ['', [Validators.required]],
-      nombre:['',[Validators.required]],
-      fecNacimiento:['',[Validators.required]],
-      direccion:['',[Validators.required]],
-      correo:['',[Validators.required,Validators.email]],
-      password: ['', [Validators.required]],
-      confirmPassword:['',[Validators.required]]
-    });
+    this.registroForm = this.fb.group(
+      {
+        username: ['', [Validators.required]],
+        nombre: ['', [Validators.required]],
+        fecNacimiento: ['', [Validators.required, minAgeValidator(13)]],
+        direccion: [''],
+        correo: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(18),
+          ],
+        ],
+        confirmPassword: ['', [Validators.required]],
+      },
+      {
+        validators: passwordsIgualesValidator,
+      }
+    );
   }
 
-  onSubmit(){
-    if(this.registroForm.valid ){
-      const { username, nombre,fecNacimiento,direccion,correo,password } = this.registroForm.value;
-      if(this.authService.registrarUsuario(username,nombre,fecNacimiento,direccion,correo,password)){
+  onSubmit() {
+    if (this.registroForm.valid) {
+      const { username, nombre, fecNacimiento, direccion, correo, password } =
+        this.registroForm.value;
+      if (
+        this.authService.registrarUsuario(
+          username,
+          nombre,
+          fecNacimiento,
+          direccion,
+          correo,
+          password
+        )
+      ) {
         Swal.fire({
-          icon: "success",
-          title: "Registro exitoso",
-          text: "Usuario registrado correctamente.",
-          confirmButtonText: "Aceptar"
-      }).then(() => {
+          icon: 'success',
+          title: 'Registro exitoso',
+          text: 'Usuario registrado correctamente.',
+          confirmButtonText: 'Aceptar',
+        }).then(() => {
           this.registroForm.reset();
-      });
+        });
       }
-
-    }else{
+    } else {
       this.registroForm.markAllAsTouched();
     }
-    
   }
   get f() {
     return this.registroForm.controls;
+  }
+  get passwordsNoSonIguales() {
+    return (
+      this.registroForm.hasError('notSame') && this.f['confirmPassword'].touched
+    );
   }
 }
