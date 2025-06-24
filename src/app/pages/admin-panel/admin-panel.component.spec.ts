38e513ba4c8157cba3a521da476454cada28bb04
valdo0 <sebastian.valdivia97gmail.com>
2025-06-23 20:15:13 -0400

    Se añaden pruebas unitarias para AdminPanelComponent con módulos y proveedores necesarios

--- src/app/pages/admin-panel/admin-panel.component.spec.ts
+++ src/app/pages/admin-panel/admin-panel.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminPanelComponent } from './admin-panel.component';
+import { ComponentsModule } from 'src/app/components/layout/components.module';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 describe('AdminPanelComponent', () => {
   let component: AdminPanelComponent;
@@ -8,7 +12,22 @@ describe('AdminPanelComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AdminPanelComponent]
+      declarations: [AdminPanelComponent],
+      imports: [ComponentsModule,FormsModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: (key: string) => null,
+              },
+            },
+            params: of({}),
+            queryParams: of({}),
+          },
+        },
+      ],
     });
     fixture = TestBed.createComponent(AdminPanelComponent);
     component = fixture.componentInstance;
